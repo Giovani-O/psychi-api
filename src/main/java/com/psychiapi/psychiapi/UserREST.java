580e74b47db70f639dc1bbffa624934f949efea1
Giovani O <giovani.o@outlook.com.br>
2021-10-27 12:19:54 -0300

    feat(Psychi API): Define as rotas da API e connection string

--- src/main/java/com/psychiapi/psychiapi/UserREST.java
+++ src/main/java/com/psychiapi/psychiapi/UserREST.java
@@ -6,25 +6,39 @@ import com.psychiapi.psychiapi.database.UserRepository;
 import com.psychiapi.psychiapi.user.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// Server path: localhost:3090
+@RestController
+@RequestMapping("/User")
 public class UserREST {
   @Autowired
   private UserRepository lRepository;
 
+  @GetMapping("/GetUsers")
   public List<User> GetUsers(){
     return lRepository.findAll();
   }
 
-  public void SaveUser(User pUser) {
+  @PostMapping("/SaveUser")
+  public void SaveUser(@RequestBody User pUser) {
     lRepository.save(pUser);
   }
 
-  public void UpdateUser(User pUser) {
+  @PutMapping("/UpdateUser")
+  public void UpdateUser(@RequestBody User pUser) {
     if (pUser.getId() > 0 && pUser.getId() != null)
       lRepository.save(pUser);
   }
 
-  public void DeleteUser(User pUser){
+  @DeleteMapping("/DeleteUser")
+  public void DeleteUser(@RequestBody User pUser){
     lRepository.delete(pUser);
   }
   
